0433030c269bb361e1c5c40657edae1ab0e1c6ce
KirstenZimbardi <kirsten.zimbardi@gmail.com>
2016-05-25 13:48:06 +1000

    Broke importAcP function
    Set up sandbox, played with import file wizard, generated function and
    tried to adapt but changes have broken the function so need to
    regenerate and see what the corrections are…

---
+++ MatLabSandbox/UQMnn.m
@@ -0,0 +1,6 @@
+%% import data
+
+path = '/Users/KirstenZ/Dropbox/UQ/FeedbackAnalytics/Usage paper/Analysis/Data Stage 2/'
+f = strcat(path, 'AcPdf.csv')
+
+%AcP = csvread('AcPdf.csv');
---
+++ MatLabSandbox/codeAnalyserReport.m
@@ -0,0 +1,3 @@
+%% import data
+
+%AcP = csvread('AcPdf.csv');
